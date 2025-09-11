83fd98fb6e89d155fe9076e655539ea86f587b0d
xavierbm2110 <xavier.barriga@uees.edu.ec>
2025-09-11 17:35:54 -0500

    Add files via upload

---
+++ Taller 3 Lenguajes.pl
@@ -0,0 +1,38 @@
+tiene(juan, bicicleta).
+tiene(juan, coche).
+tiene(ana , coche).
+tiene(pedro,salchipapa).
+cuadrado(X,R):- R is X*X.
+media(A,B,M):- M is(A+B)/2.
+
+factorial(0,1).
+factorial(N,R):-
+    N1 is N - 1,
+    factorial(N1,R1),
+    R is N * R1.
+
+poblacion('Francia',60000000).
+poblacion('España',45000000).
+area('Francia',640000).
+area('España',505000).
+densidad(Pais,D):-
+    poblacion(Pais,P),
+    area(Pais,A),
+    D is P / A.
+
+mcd(X,0,X).
+mcd(X,Y,M):-
+    X1 is X mod Y ,
+    mcd(Y,X1,M).
+
+fibonacci(0,1).
+fibonacci(1,1).
+fibonacci(N,R) :-
+    N > 1,
+    N1 is N-1,
+    N2 is N-2,
+    fibonacci(N1,R1),
+    fibonacci(N2,R2),
+    R is R1 + R2.
+
+
